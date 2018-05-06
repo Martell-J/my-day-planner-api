3b096ef52fb165452d26f703277496db9eb39a43
Johnathan Martell <Martell-J@users.noreply.github.com>
2018-05-06 00:31:56 -0400

    Allow a user with a token in the header to verify their credentials

---
+++ api/controllers/user.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const { models } = require("../../app");
+
+module.exports = {
+
+  "getUserByAuthorization": (req, res) => {
+
+    models.User.find({
+      "where": {
+        "user_id": req.authorization.uid,
+      },
+      "attributes": [ "user_id", "username", "email", "first_name", "last_name" ],
+    })
+      .then((results) => res.json({ "user": { ...results.get() }, "exp": req.authorization.exp, "iat": req.authorization.iat }))
+      .catch((err) => {
+
+        return res.status(400).json({
+          "name": err.name,
+          "message": err.message,
+        });
+
+      });
+
+
+  },
+
+};
