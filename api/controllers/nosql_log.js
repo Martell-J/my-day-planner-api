fdbdd9716028094a407028159b7b6f0f451cc204
John <Martell-J@users.noreply.github.com>
2019-05-09 21:49:19 -0400

    Fixed error logging.

--- api/controllers/nosql_log.js
+++ api/controllers/nosql_log.js
@@ -1,19 +1,19 @@
 "use strict";
 
 const { mongoose } = require("../../app");
-const ErrorModel = mongoose.Error;
+const LogModel = mongoose.Log;
 
 const { sendError } = require("../helpers/errorhelper");
 const { getWithPagination } = require("../helpers/pagination");
 
 module.exports = {
 
-  "getAllErrorLogs": (req, res) => {
+  "getAllLogs": (req, res) => {
 
     const count = req.swagger.params.count.value;
     const page = req.swagger.params.page.value;
 
-    getWithPagination(ErrorModel, "find", page, count, "mongoose")
+    getWithPagination(LogModel, "find", page, count, "mongoose")
       .then((results) => {
 
         return res.status(200).json(results);
